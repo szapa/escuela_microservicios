ed8091c8a48d2c0b2f866e6809ecbface48418bc
Sergio Z <s.zapatero.mar@gmail.com>
2019-12-16 15:55:41 +0100

    Añadiendo Timestamp

--- api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/entidades/Usuario.java
+++ api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/entidades/Usuario.java
@@ -1,11 +1,20 @@
 package com.viewnext.apiusuarios.entidades;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.springframework.lang.NonNull;
 
 @Entity
 public class Usuario implements Serializable {
@@ -16,8 +25,22 @@ public class Usuario implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotNull
+	@Size(min = 1, max = 50)
 	private String nombre;
+	
+	@NonNull
+	@Size(min = 3, max = 500)
+	@Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")
 	private String email;
+	
+	// Información específica de la columna en la bbdd
+	@Column(name = "timestamp", nullable = false, updatable = false,
+			insertable = false, columnDefinition = "TIMESTAMP")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date timestamp;
+	
+	@Size(min = 2, max = 500)
 	private String password;
 	
 	public Usuario(Integer id, String nombre, String email, String password) {
