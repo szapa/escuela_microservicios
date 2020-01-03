c4b46dd500508b07a87acc684bdcf096bab41643
Sergio Z <s.zapatero.mar@gmail.com>
2020-01-03 12:12:22 +0100

    Cambios nueva y vieja

--- api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/restapi/TemasController.java
+++ api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/restapi/TemasController.java
@@ -3,6 +3,7 @@ package com.viewnext.apiusuarios.restapi;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.viewnext.apiusuarios.model.AlmacenDAOTemas;
 
 @RestController
 @RequestMapping("/api/temas")
+@CrossOrigin()
 public class TemasController {
 
 	@Autowired
--- api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/restapi/UsuariosJsonController.java
+++ api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/restapi/UsuariosJsonController.java
@@ -3,6 +3,7 @@ package com.viewnext.apiusuarios.restapi;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.viewnext.apiusuarios.model.AlmacenDAOUsuarios;
 
 @RestController()
 @RequestMapping("/api/json/usuarios")
+@CrossOrigin()
 public class UsuariosJsonController {
 
 	@Autowired
